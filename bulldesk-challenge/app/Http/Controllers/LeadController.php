5e94b99a537ded18e5ae247248cafa00c4e0094e
Matheus-note <matheuscarvalho.mcdc@gmail.com>
2020-01-12 22:48:18 -0300

    importando csv para o banco de dados atraves de filas com redis; criacao do arquivo start_queue.bat para iniciar o listener das queues automaticamente; inclusao do arquivo small-leads.csv para facilitar os testes

--- bulldesk-challenge/app/Http/Controllers/LeadController.php
+++ bulldesk-challenge/app/Http/Controllers/LeadController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ImportLead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LeadController extends Controller
 {
     public function importLeads(Request $request)
     {
-
+        $leads = $request->get('csv');
+        $user_id = Auth::user()->id;
+        foreach ($leads as $key => $lead) {
+            if ($key == count($leads) - 1) {
+                ImportLead::dispatch($lead, $user_id, $key, true);
+            } else {
+                ImportLead::dispatch($lead, $user_id, $key);
+            }
+        }
     }
 }
---
+++ bulldesk-challenge/app/Jobs/ImportLead.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Lead;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class ImportLead implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $lead;
+    public $user_id;
+    public $last;
+    public $index;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param array $lead
+     * @param int $user_id
+     * @param int $index
+     * @param bool $last
+     *
+     * @return void
+     */
+    public function __construct(array $lead, int $user_id, int $index, bool $last = false)
+    {
+        $this->lead = $lead;
+        $this->user_id = $user_id;
+        $this->index = $index;
+        $this->last = $last;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Lead::create($this->lead);
+        if ($this->last === true) {
+            $user = Auth::loginUsingId($this->user_id);
+            Log::info('ultimo registro' . $user);
+        }
+    }
+}
